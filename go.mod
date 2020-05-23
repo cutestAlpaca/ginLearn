843d755209ce866cc68cfe24324285d4f0eb507d
“用koko <1161097471@qq.com>
2020-05-23 20:59:57 +0800

    first

---
+++ go.mod
@@ -0,0 +1,15 @@
+module ginLearn
+
+go 1.13
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-playground/validator/v10 v10.3.0 // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/jinzhu/gorm v1.9.12
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	golang.org/x/sys v0.0.0-20200519105757-fe76b779f299 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+type User struct {
+	gorm.Model
+	Name      string `gorm:"type:varchar(20);not null"`
+	Telephone string `gorm:"type:varchar(20);not null;unique"`
+	Password  string `gorm:"size:255;not null"`
+}
+
+func main() {
+	db := InitDB()
+	defer db.Close()
+
+	r := gin.Default()
+	r.POST("/api/auth/register", func(c *gin.Context) {
+		name := c.PostForm("name")
+		telephone := c.PostForm("telephone")
+		password := c.PostForm("password")
+
+		if len(telephone) != 11 {
+			c.JSON(http.StatusUnprocessableEntity, map[string]interface{}{ //gin.H
+				"code": 442,
+				"msg":  "手机号错误,手机号必须为11位",
+			})
+			return
+		}
+
+		if len(password) < 6 {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{
+				"code": 442,
+				"msg":  "手机号错误,手机号必须为11位",
+			})
+			return
+		}
+
+		if len(name) == 0 {
+			name = RandomString(10)
+		}
+
+		if isTelephoneExist(db, telephone) {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{
+				"code": 442,
+				"msg":  "用户已存在",
+			})
+			return
+		}
+
+		newUser := User{
+			Name:      name,
+			Telephone: telephone,
+			Password:  password,
+		}
+		db.Create(&newUser)
+
+		log.Println(name, telephone, password)
+
+		c.JSON(200, gin.H{
+			"msg": "注册成功!",
+		})
+	})
+	_ = r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func isTelephoneExist(db *gorm.DB, telephone string) bool {
+	var user User
+	db.Where("telephone = ?", telephone).First(&user)
+
+	if user.ID != 0 {
+		return true
+	}
+
+	return false
+}
+
+func RandomString(n int) string {
+	var letters = []byte("abcdefghtyuikllwetyibvhjigkgggkgju")
+	result := make([]byte, n)
+	// 设置种子，不然每次都会随机成0
+	rand.Seed(time.Now().Unix())
+
+	for i := range result {
+		// 生成 letters的长度(n) 到 (n-1) 的随机数（输出 int类型）
+		result[i] = letters[rand.Intn(len(letters))]
+	}
+
+	return string(result)
+}
+
+func InitDB() *gorm.DB {
+	driverName := "mysql"
+	host := "localhost"
+	port := "3306"
+	database := "ginessential"
+	username := "root"
+	password := "root"
+	charset := "utf8"
+	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true",
+		username,
+		password,
+		host,
+		port,
+		database,
+		charset,
+	)
+	db, err := gorm.Open(driverName, args)
+	if err != nil {
+		panic("连接数据库失败" + err.Error())
+	}
+	log.Println(db.HasTable("users"))
+	db.AutoMigrate(&User{})
+
+	return db
+}
